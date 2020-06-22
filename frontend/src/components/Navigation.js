36ac326c16a178d844f215daa676b1bb6a479c96
Andrew Hewitt <andrew@yohdigital.com>
2020-06-22 18:09:05 -0400

    contact path

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -11,6 +11,7 @@ const Navigation = () => (
                 <Nav className="ml-auto">
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/page">Page</Nav.Link>
+                    <Nav.Link href="/contact">Contact</Nav.Link>
                 </Nav>
               </Navbar.Collapse>
               </Container>

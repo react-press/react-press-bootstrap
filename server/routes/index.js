d99be9b170dd05f8a052566d34863e2c1d39d652
Andrew Hewitt <andrew@yohdigital.com>
2020-06-25 00:14:25 -0400

    proxy etc...

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,8 +10,4 @@ router.get('/test/:id', function(req, res, next) {
   res.render('test', {output: req.params.id});
 });
 
-router.post('/test/submit', function(req, res, next) {
-  res.redirect('/test/...');
-});
-
 module.exports = router;

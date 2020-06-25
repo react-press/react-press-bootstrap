bf9862e704273a7c7157e30b66794a397cb4a721
ayohewitt <andrew@yohdigital.com>
2020-06-25 05:19:03 +0000

    express working on server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   state = { users: [] }
 
   componentDidMount() {
-    fetch('/users')
+    fetch('/express/users')
       .then(res => res.json())
       .then(users => this.setState({ users }));
   }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cool, huh!', condition: true, anyArray: ['token',2,3] });
+router.get('/express', function(req, res, next) {
+  res.render('index', { title: 'Cool, huh!', condition: true, anyArray: [1,2,3] });
 });
 
 router.get('/test/:id', function(req, res, next) {
   res.render('test', {output: req.params.id});
 });
+
 
 module.exports = router;

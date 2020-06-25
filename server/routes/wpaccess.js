6af4dd2f4d5d37f2fdbbb8f0af6781e400a50e10
ayohewitt <andrew@yohdigital.com>
2020-06-25 18:13:31 +0000

    secured express routes

--- server/routes/wpaccess.js
+++ server/routes/wpaccess.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'NO ACCESS!', condition: true, anyArray: [1,2,3] })
   res.json({
     wpAccessToken: process.env.SECRET_KEY,
     wpUrl: process.env.URL

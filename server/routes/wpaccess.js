3b537dc4ebbf9fadeedadb912249c67820ada6f1
Andrew Hewitt <andrew@yohdigital.com>
2020-06-25 13:43:00 -0400

    added dotenv

--- server/routes/wpaccess.js
+++ server/routes/wpaccess.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
-    wpAccessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hZG1pbi5yZWFjdC1wcmVzcy5uZXQiLCJpYXQiOjE1OTMwOTgyMTksIm5iZiI6MTU5MzA5ODIxOSwiZXhwIjoxNTkzNzAzMDE5LCJkYXRhIjp7InVzZXIiOnsiaWQiOjF9fX0.yPCoXPQvXCkwqbHdlBBxuP13IS-k16SHx3ulAzeX7qs',
-    wpUrl: 'https://admin.react-press.net/wp-json'
+    wpAccessToken: process.env.SECRET_KEY,
+    wpUrl: process.env.URL
   })
 });
 
